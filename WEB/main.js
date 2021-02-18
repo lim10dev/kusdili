09a39460d69b6ff4f8722625c502f2b57f79af98
lim10dev <60268293+lim10dev@users.noreply.github.com>
2021-02-18 23:42:13 +0300

    Depolama güncellemesi
    -Ayarlar saklanabiliyor
    -Tasarım değişiklikleri

--- WEB/main.js
+++ WEB/main.js
@@ -6,38 +6,117 @@ var cevirMod = document.getElementById("CevirMod");
 var cevirHarf = document.getElementById("CevirHarf");
 var cvrButon = document.getElementById("Cevir");
 var kopyala = document.getElementById("Kopyala");
+var kaydet = document.getElementById("Kaydet");
+var ters = document.getElementById("Ters");
+var kaydet_hatirla = document.getElementById("Kaydet-Hatirla");
 }
 
+addEventListener("load", function(){
+    
+    console.log("Ayarlar kontrol ediliyor...")
+    if(localStorage.getItem("ayarlar") != null && localStorage.getItem("hatirla") == "true"){
+
+        console.log("Ayarlar tespit edildi. Ayarlar yükleniyor...");
+        cevirMod.value = localStorage.getItem("ayarlar").split(";")[0];
+        cevirHarf.value = localStorage.getItem("ayarlar").split(";")[1]
+
+    }
+
+    kaydet_hatirla.checked = (localStorage.getItem("hatirla").toLowerCase() == "true");
+})
+
 cvrButon.addEventListener("click", function(){
-    var sesliHarfler = ["a","e","ı","i","u","ü","o","ö"];
+
+    if(cevrilen.value == "") {
+        
+    console.error("Boş metin çevrilemez.");
+    return;
+
+    }
+    console.log("Çevrilmeye başlanıyor...");
+    var kucukSesliHarfler = ["a","e","ı","i","u","ü","o","ö"];
+    var buyukSesliHarfler = ["A", "E", "I", "İ", "U", "Ü", "O", "Ö"];
     ceviri.value = "";
     if(cevirMod.value == "Turkce"){
         // Türkçe'den Kuş Dili'ne
+
         for(i = 0; i < cevrilen.value.length; i++){
-            if(sesliHarfler.includes(cevrilen.value.charAt(i).toLowerCase())){
-                if(sesliHarfler.includes(cevrilen.value.charAt(i))){
+
+            if(kucukSesliHarfler.includes(cevrilen.value.charAt(i)) || buyukSesliHarfler.includes(cevrilen.value.charAt(i))){
+
+                if(kucukSesliHarfler.includes(cevrilen.value.charAt(i))){
                     // Eğer küçük harfse
+
                 ceviri.value += cevrilen.value.charAt(i) + cevirHarf.value + cevrilen.value.charAt(i);
+
                 } else {
+
                     ceviri.value += cevrilen.value.charAt(i) + cevirHarf.value.toUpperCase() + cevrilen.value.charAt(i);
+
                 }
             } else {
+
                 ceviri.value += cevrilen.value.charAt(i);
+
             }
+
         }
+
     } else {
         // Kuş Dili'nden Türkçe'ye
+
         for(i = 0; i < cevrilen.value.length; i++){
+
             ceviri.value += cevrilen.value.charAt(i);
-            if(sesliHarfler.includes(cevrilen.value.charAt(i).toLowerCase())){
+            if(kucukSesliHarfler.includes(cevrilen.value.charAt(i).toLowerCase())){
+
                 i+= cevirHarf.value.length + 1;
+
             }
+
         }
+
     }
     kopyala.disabled = (ceviri.value == "")
+
 });
 
 kopyala.addEventListener("click", function(){
+
+    console.log("Çeviri kopyalanıyor...");
     ceviri.select(); ceviri.setSelectionRange(0, 99999);
     document.execCommand("copy");
+
 });
+
+document.onkeyup = function(ev){
+
+    if(ev.code == "Enter" && (document.activeElement == ceviri))
+    cvrButon.click();
+
+}
+
+kaydet.addEventListener("click", function(){
+
+    console.log("Tercihler kaydediliyor...");
+    localStorage.setItem("ayarlar", `${cevirMod.value};${cevirHarf.value}`);
+
+});
+
+ters.addEventListener("click", function(){
+
+    var cevrilenYeni = ceviri.value;
+    var ceviriYeni = cevrilen.value;
+
+    cevrilen.value = cevrilenYeni;
+    ceviri.value = ceviriYeni;
+    
+    kopyala.disabled = (ceviri.value == "")
+
+})
+
+kaydet_hatirla.addEventListener("change", function(){
+
+    localStorage.setItem("hatirla", kaydet_hatirla.checked);  
+
+})
